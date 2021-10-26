e28a7aad52dabf075027fac1f9edc61bbf6e9253
Cathy Chi <cathy@webigence.com>
2021-10-26 18:10:58 +0100

    Added destination-card

---
+++ src/app/destination-card/destination-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'destination-card',
+  templateUrl: './destination-card.component.html',
+  styleUrls: ['./destination-card.component.css']
+})
+export class DestinationCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
